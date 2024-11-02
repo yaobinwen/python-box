55559e7a697a97f6c9410dc29f7abf774606d291
Yaobin Wen <robin.wyb@gmail.com>
2024-11-02 14:38:07 -0400

    Built-in types: Update demo code

--- py3/built-ins/types_demo.py
+++ py3/built-ins/types_demo.py
@@ -12,19 +12,55 @@ class Test_bytearray(unittest.TestCase):
     to `bytes` objects.
     """
 
-    def test_bytearray(self):
+    def test_bytearray_empty(self):
+        """Test case: `source` is not provided.
+        """
         # Create an empty instance.
         ba = bytearray()
         self.assertEqual(len(ba), 0)
 
-        # If `source` is an integer, it indicates the expected length of the
-        # bytearray.
+    def test_bytearray_integer(self):
+        """Test case: `source` is an integer which indicates the expected
+        length of the bytearray.
+        """
         ba = bytearray(10)
         self.assertEqual(len(ba), 10)
         for b in ba:
             # Every element in the bytearray is zero (i.e., null byte).
             self.assertEqual(b, 0)
 
+        # If provided with zero, it's the same as creating an empty bytearray.
+        ba = bytearray(0)
+        self.assertEqual(len(ba), 0)
+
+        # If provided a negative integer, it raises `ValueError`.
+        self.assertRaisesRegex(
+            ValueError,
+            r"negative count",
+            bytearray,
+            source=-1,
+        )
+
+    def test_bytearray_range(self):
+        # If `source` is a list of integers (0 <= n <= 255), then the bytearray
+        # is initialized from the list of integers.
+        ba = bytearray(range(256))
+        self.assertEqual(len(ba), 256)
+        for b, n in zip(ba, range(256)):
+            # Every element in the bytearray is the corresponding integer.
+            self.assertEqual(b, n)
+
+        # If the given integer is out of range, `ValueError` is raised.
+        for n in (-1, 256):
+            with self.subTest(n=n):
+                self.assertRaisesRegex(
+                    ValueError,
+                    r"byte must be in range\(0, 256\)",
+                    bytearray,
+                    source=[n],
+                )
+
+    def test_bytearray_str(self):
         # If `source` is a string but `encoding` is not provided, a `TypeError`
         # will be raised.
         self.assertRaisesRegex(
@@ -88,6 +124,10 @@ class Test_bytearray(unittest.TestCase):
                 self.assertEqual(ba[i], replace_char[i])
 
         _err_handling_scheme_replace()
+
+    def test_bytearray_buffer(self):
+        ba = bytearray(b'hello')
+        pass
 
 
 if __name__ == "__main__":
